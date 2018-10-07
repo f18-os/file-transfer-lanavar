e4f03b2b30a21e6f8cc5b5b29b234a4f232e3ad5
lanavar <lanavar@miners.utep.edu>
2018-10-06 22:59:29 -0600

    INitial commit for working echo server

---
+++ file-transfer-lab/fileClient.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python3
+
+#File transfer client program
+import socket, sys, re
+sys.path.append("../lib")    # for params
+import params
+
+#First get input from user
+command = input("Please enter the command for file transfer. ")
+
+print ("Command to transmit is '%s'" % command)
+size = sys.getsizeof(command)
+print ("Size in bytes is '%d'" % size)
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-?', '--usage'), "usage", False), #boolean (set if present)
+    )
+
+progname = "framedClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage = paramMap["server"], paramMap["usage"]
+
+if usage:
+    params.usage()
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print ("Can't parse server: port from '%s'" % server)
+    sys.exit(1)
+
+s = None
+for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s = None
+        continue
+    try:
+        print(" attempting to connect to %s" % repr(sa))
+        s.connect(sa)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s.close()
+        s = None
+        continue
+    break
+
+if s is None:
+    print("Could not open Socket")
+    sys.exit(1)
+
+s.send(command.encode())
+
+data = s.recv(1024).decode()
+print("Received '%s'" % data)
+print("Sending '%s'" % command)
+
+s.send(command.encode())
+
+s.shutdown(socket.SHUT_WR) # no more ouput
+
+while 1:
+    data = s.recv(1024).decode()
+    print("Received '%s'" % data)
+    if len(data) == 0:
+        break
+print("Zero length read. Closing")
+s.close()
